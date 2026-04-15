0bc7f926841d0a113eab4f34fb69b9a7298a7982
Annu9111 <aksoni911190@gmail.com>
2026-04-15 20:55:53 +0530

    urls

--- books/views.py
+++ books/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+
+# GET all books
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+# GET single book
+@api_view(['GET'])
+def get_book(request, id):
+    try:
+        book = Book.objects.get(id=id)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+    except Book.DoesNotExist:
+        return Response({"error": "Book not found"})
