58dff3f3d4da2a56f7668f6ff3bfd63845a29e19
Annu9111 <aksoni911190@gmail.com>
2026-04-16 19:27:08 +0530

    commit

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('books/', views.get_books),
     path('books/<int:id>/', views.get_book),
+    path('books/add/', views.add_book),
+    path('scrape/', views.scrape_books_api),
 ]
--- books/views.py
+++ books/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Book
 from .serializers import BookSerializer
+from .scraper import scrape_books
+
 
 # GET all books
 @api_view(['GET'])
@@ -12,6 +13,7 @@ def get_books(request):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
+
 
 # GET single book
 @api_view(['GET'])
@@ -21,4 +23,35 @@ def get_book(request, id):
         serializer = BookSerializer(book)
         return Response(serializer.data)
     except Book.DoesNotExist:
-        return Response({"error": "Book not found"})
+        return Response({"error": "Book not found"}, status=404)
+
+
+# SCRAPE BOOKS (IMPORTANT)
+@api_view(['GET'])
+def scrape_books_api(request):
+    books_data = scrape_books()
+
+    created_books = []
+
+    for book in books_data:
+        serializer = BookSerializer(data=book)
+        if serializer.is_valid():
+            serializer.save()
+            created_books.append(serializer.data)
+
+    return Response({
+        "message": "Books scraped and saved successfully!",
+        "total_added": len(created_books)
+    })
+
+
+# ADD BOOK (POST API)
+@api_view(['POST'])
+def add_book(request):
+    serializer = BookSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+    return Response(serializer.errors, status=400)
