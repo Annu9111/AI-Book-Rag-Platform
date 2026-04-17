d5099876da3fc0393900d407861439918aafc73b
Annu9111 <aksoni911190@gmail.com>
2026-04-17 07:14:00 +0530

    commit

--- books/urls.py
+++ books/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
+
     path('books/', views.get_books),
     path('books/<int:id>/', views.get_book),
     path('books/add/', views.add_book),
     path('scrape/', views.scrape_books_api),
-    path('ask/', views.ask_books),
-    path('', views.home),
+
+    path('ask/', views.ask_books),  
 ]
